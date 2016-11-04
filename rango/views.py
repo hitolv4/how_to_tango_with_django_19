276e7fa81534e1c2afbadf1943fc41dcadf2e596
victor <victorravelolv4@gmail.com>
2016-11-04 12:14:43 -0400

    templates and media

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse('Rango says hey there partner')
+	# Construct a dictionary to pass to the template engine as its context.
+	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
+	#crea un diccionario para pasar al motor template como su contexto
+	# nota que la clave blodmessage es la mismaque {{ bolmessage }}enel template
+	context_dict={'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	return render(request, 'rango/index.html',context=context_dict)
 
 def rangoindex(request):
 	return HttpResponse('<h1>Rango index</h1>')
